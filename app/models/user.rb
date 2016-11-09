9d728feb295d540b48e25960d0bc84b12b392973
Tommy Hui <hui.1tommy@gmail.com>
2016-11-09 18:12:09 -0500

    added user name to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,28 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :login
+
+  has_one :portfolio
+  has_one :watch_list
+  has_many :properties, through: :portfolio
+  has_many :properties, through: :watch_list
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
 end
